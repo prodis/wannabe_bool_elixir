5a10db6a3ab87ec8420cf60ce21bf97a02ecd619
Prodis <prodis@gmail.com>
2020-04-20 00:10:07 +0200

    Update dependencies.

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule WannabeBool.MixProject do
   defp deps do
     [
       # Development
-      {:credo, "~> 1.3", only: :dev, runtime: false},
+      {:credo, "~> 1.4", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
 
