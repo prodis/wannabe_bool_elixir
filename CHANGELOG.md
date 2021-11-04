2d756be912269e60531e2ba70f63a3e6e391eb75
Prodis <prodis@gmail.com>
2021-11-04 12:24:09 +0100

    Update development and test dependencies.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 All notable changes to this project will be documented in this file.
 
 ## [Unreleased]
+### Changed
+- Update development and test dependencies.
 
 ## [0.1.2] - 2020-04-13
 ### Fixed
--- mix.exs
+++ mix.exs
@@ -1,47 +1,39 @@
 defmodule WannabeBool.MixProject do
   use Mix.Project
 
+  @app :wannabe_bool
+  @name "Wannabe Bool"
+  @repo "https://github.com/prodis/wannabe_bool_elixir"
   @version "0.1.2"
-  @github_url "https://github.com/prodis/wannabe_bool_elixir"
 
   def project do
     [
-      app: :wannabe_bool,
-      name: "Wannabe Bool",
+      app: @app,
+      name: @name,
       version: @version,
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: dialyzer(),
       description: description(),
-      package: package(),
       docs: docs(),
-      test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [
-        coveralls: :test,
-        "coveralls.detail": :test,
-        "coveralls.post": :test,
-        "coveralls.html": :test,
-        "coveralls.travis": :test
-      ]
+      package: package(),
+      preferred_cli_env: preferred_cli_env(),
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
-  def application do
-    [
-      extra_applications: [:logger]
-    ]
-  end
+  def application, do: []
 
   defp deps do
     [
       # Development
-      {:credo, "~> 1.3", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
-      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:credo, "~> 1.5", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.1.0", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.25", only: :dev, runtime: false},
 
       # Test
-      {:excoveralls, "~> 0.12", only: :test}
+      {:excoveralls, "~> 0.14", only: :test}
     ]
   end
 
@@ -57,22 +49,33 @@ defmodule WannabeBool.MixProject do
     ]
   end
 
+  defp docs() do
+    [
+      main: "readme",
+      extras: ~w(README.md CHANGELOG.md),
+      formatters: ~w(html),
+      source_ref: @version,
+      source_url: @repo,
+      canonical: "http://hexdocs.pm/wannabe_bool"
+    ]
+  end
+
   defp package do
     [
       files: ~w(lib mix.exs README.md CHANGELOG.md LICENSE),
       maintainers: ["Fernando Hamasaki de Amorim"],
       licenses: ["Apache 2.0"],
-      links: %{"GitHub" => @github_url}
+      links: %{"GitHub" => @repo}
     ]
   end
 
-  defp docs() do
+  defp preferred_cli_env do
     [
-      main: "readme",
-      extras: ~w(README.md CHANGELOG.md),
-      source_ref: @version,
-      source_url: @github_url,
-      canonical: "http://hexdocs.pm/wannabe_bool"
+      coveralls: :test,
+      "coveralls.detail": :test,
+      "coveralls.post": :test,
+      "coveralls.html": :test,
+      "coveralls.travis": :test
     ]
   end
 end
